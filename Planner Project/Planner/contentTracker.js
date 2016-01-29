46263b984c621a5afcc93de1e628e772b201364c
sophia_de <sdesmarais.sp@mysummitps.org>
2016-01-29 19:52:43 +0000

    Nolan

---
+++ Planner Project/Planner/contentTracker.js
@@ -0,0 +1,33 @@
+  /* connects the HTML file to this JavaScript file 
+  and defines the variables that are taken from the HTML file*/
+
+  var gradeChoicesDropdown = document.getElementById('dropdown'),
+    caInputBox = document.getElementById('inputBox'),
+    compGeneratedResponse = document.getElementById('compResponse'),
+    calculateButton = document.getElementById('submitButton');
+
+  /*Event Listener tells the program that when the button is clicked, preform the function*/
+
+  calculateButton.addEventListener('click', function(evt) {
+
+
+    var userInput = caInputBox.value;
+    var outcome = Math.round(56 - userInput); //This says to take the input that the user writes and subtract 56 from it
+    if (gradeChoicesDropdown.value == "fresh") {
+      compGeneratedResponse.innerHTML = 'You have ' + outcome + ' Content Assessments to pass to finish the year. <br> That means you have to pass about ' + Math.round(outcome / 23) + ' Content Assessment(s) per week to pass the year!';
+    }
+    else if (gradeChoicesDropdown.value == "soph") {
+      compGeneratedResponse.innerHTML = 'You have ' + outcome + ' Content Assessments to pass to finish the year.<br> That means you have to pass about ' + Math.round(outcome / 23) + ' Content Assessment(s) per week to pass the year!';
+    }
+    else if (gradeChoicesDropdown.value == "jr") {
+      var outcomeJr = Math.round(69 - userInput); //The 56 value has changed because the number of CAs for Juniors is more
+      compGeneratedResponse.innerHTML = 'You have ' + outcomeJr + ' Content Assessments to pass to finish the year.<br> That means you have to pass about ' + Math.round(outcomeJr / 23) + ' Content Assessment(s) per week to pass the year!';
+    }
+    else if (gradeChoicesDropdown.value == "snr") {
+      var outcomeSnr = Math.round(77 - userInput); //The 69 value has changed because the number of CAs for Seniors is more
+      compGeneratedResponse.innerHTML = 'You have ' + outcomeSnr + ' Content Assessments to pass to finish the year. <br>That means you have to pass about ' + Math.round(outcomeSnr / 23) + ' Content Assessment(s) per week to pass the year!';
+
+    }
+    evt.preventDefault();
+  }, false);
+  
---
+++ Planner Project/Planner/planner.js
@@ -0,0 +1,114 @@
+/* connects the HTML file to this JavaScript file 
+and defines the variables that are taken from the HTML file*/
+
+var todo = document.getElementById('planner'),
+  form = document.getElementById('myform'),
+  newItemInput = document.getElementById('newitem'),
+  dropdownColorOptions = document.getElementById('dropdown'),
+  dropdownSubjectOptions = document.getElementById('dropdown2'),
+  mathTasks = document.getElementById('mathSection'),
+  scienceTasks = document.getElementById('scienceSection'),
+  historyTasks = document.getElementById('historySection'),
+  englishTasks = document.getElementById('englishSection'),
+  foreignLangTasks = document.getElementById('foreignLanguageSection');
+
+
+//tells program what to do when each subject value is selected
+/*Event Listener tells the program that when the button is clicked, preform the function*/
+form.addEventListener('submit', function(evt) {
+  var text = newItemInput.value;
+  
+  if(dropdownSubjectOptions.value == "math") {
+    mathTasks.innerHTML += '<p style="color:' + dropdownColorOptions.value + '">' + text + '</p>';
+  }
+  else if(dropdownSubjectOptions.value == "science") {
+    scienceTasks.innerHTML += '<p style="color:' + dropdownColorOptions.value + '">' + text + '</p>';
+  }
+  else if(dropdownSubjectOptions.value == "history") {
+    historyTasks.innerHTML += '<p style="color:' + dropdownColorOptions.value + '">' + text + '</p>';
+  }
+  else if(dropdownSubjectOptions.value == "english") {
+    englishTasks.innerHTML += '<p style="color:' + dropdownColorOptions.value + '">' + text + '</p>';
+  }
+  else if(dropdownSubjectOptions.value == "foreignLang") {
+    foreignLangTasks.innerHTML += '<p style="color:' + dropdownColorOptions.value + '">' + text + '</p>';
+  }
+  
+  evt.preventDefault();
+}, false);
+
+
+
+mathTasks.addEventListener('click', function(evt) {
+  var tar = evt.target;
+  // tells that if text is clicked it should have a strikethrough and dissappear next click
+
+  if (tar.style.textDecoration === "line-through") {
+    tar.parentNode.removeChild(tar);
+  }
+  else {
+    tar.style.textDecoration = "line-through";
+  }
+
+  evt.preventDefault();
+}, false);
+
+
+scienceTasks.addEventListener('click', function(evt) {
+  var tar = evt.target;
+  // tells that if text is clicked it should have a strikethrough and dissappear next click
+
+  if (tar.style.textDecoration === "line-through") {
+    tar.parentNode.removeChild(tar);
+  }
+  else {
+    tar.style.textDecoration = "line-through";
+  }
+
+  evt.preventDefault();
+}, false);
+
+
+historyTasks.addEventListener('click', function(evt) {
+  var tar = evt.target;
+  // tells that if text is clicked it should have a strikethrough and dissappear next click
+
+  if (tar.style.textDecoration === "line-through") {
+    tar.parentNode.removeChild(tar);
+  }
+  else {
+    tar.style.textDecoration = "line-through";
+  }
+
+  evt.preventDefault();
+}, false);
+
+
+englishTasks.addEventListener('click', function(evt) {
+  var tar = evt.target;
+  // tells that if text is clicked it should have a strikethrough and dissappear next click
+
+  if (tar.style.textDecoration === "line-through") {
+    tar.parentNode.removeChild(tar);
+  }
+  else {
+    tar.style.textDecoration = "line-through";
+  }
+
+  evt.preventDefault();
+}, false);
+
+
+foreignLangTasks.addEventListener('click', function(evt) {
+  var tar = evt.target;
+  // tells that if text is clicked it should have a strikethrough and dissappear next click
+
+  if (tar.style.textDecoration === "line-through") {
+    tar.parentNode.removeChild(tar);
+  }
+  else {
+    tar.style.textDecoration = "line-through";
+  }
+
+  evt.preventDefault();
+}, false);
